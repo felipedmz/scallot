e10b682121873be9f6fadcdf0f6cba627150d0b9
Felipe Pereira <felipe.pereira@grumft.com>
2014-12-18 18:31:58 -0200

    Reading URLs from source file

--- src/main/scala/boot/Boot.scala
+++ src/main/scala/boot/Boot.scala
@@ -7,14 +7,20 @@
 
 package boot
 
-import scalaj.http.Http
+import scalaj.http._
+import scala.io.Source
 
 object Boot {
-	var request: Http.Request = Http("http://google.com/")
-
-	def main(args : Array[String]) {
-		val result = request.asString
-		println(result)		
+	
+	def main(args: Array[String]) {
+		println("Iniciando a leitura dos arquivos")
+		for (line <- Source.fromFile("sources.txt").getLines()) {
+			val result = request(line)
+			println(result)
+		}
   	}
-    
+
+  	def request(url: String): String = {
+  		Http(url).option(HttpOptions.connTimeout(1000)).option(HttpOptions.readTimeout(5000)).asString
+  	}
 }
