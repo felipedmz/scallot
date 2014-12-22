903c6172dfad9e22cc86e703a33c17cf5c6e2b6b
Felipe Pereira <felipe.pereira@grumft.com>
2014-12-22 11:01:54 -0200

    Educational comments

--- src/main/scala/bot/Bot.scala
+++ src/main/scala/bot/Bot.scala
@@ -3,39 +3,40 @@
  * @author    Felipe Maziero <flpmaziero@gmail.com>
  * @copyright 2014 Felipe Maziero
  *
+ * Commented code for pedagogical purposes :D
  */
 
 package bot
 
-import scalaj.http._
-import scala.io.Source
+import scalaj.http._    // it's a java lib wrapped
+import scala.io.Source  // to read physical files
 
 object Bot {
 	var ways: List[String] = List()
 	var embeds: List[String] = List()
 
 	def main(args: Array[String]) {
-		println("\nIniciando a leitura das fontes primarias")
+		println("\nInitialized reading of primary sources")
 		loadPrimarySources
 
-		println("\nBuscando por embeds compatíveis")
+		println("\nSearching for compatible embeds")
 		embeds = searchForEmbeds(ways)
   	}
 
   	def searchForEmbeds(urls: List[String]): List[String] = {
-  		for (url <- ways) {
+  		for (url <- ways) { // this is like a foreach
 	  		val html  = getRequest(url)
-	  		val embed = """<embed(.)*>(.)*</embed>""".r
+	  		val embed = """<embed(.)*>(.)*</embed>""".r // this is a regexp
 			val found = (embed findAllIn html).toList
 	  		for (e <- found) {
 				println(e)
 				println("\n")
 			}
 		}
-  		urls
+  		urls // the last line is the return
   	}
 
-  	def loadPrimarySources(): Unit = {
+  	def loadPrimarySources(): Unit = { // Unit is a Scala's 'void'
   		for (line <- Source.fromFile("sources.txt").getLines()) {
 			val result = getRequest(line)
 			findLinks(result)
@@ -45,10 +46,12 @@ object Bot {
   	def findLinks(html: String): Unit = {
   		val link  = """http://[a-z0-9./?!@#$%&*)(-_=+;:,\|^~}{]*""".r
 		val found = (link findAllIn html).toList
-		ways = ways union found
+		ways = ways union found // there is a sum of lists (Yes, easy as you see ;D)
   	}
 
   	def getRequest(url: String): String = {
+  		/* How Http is a wrapped lib, this sintax sounds like Java, 
+  		   with the function returning directly the last line */
   		Http(url).option(HttpOptions.connTimeout(2000)).header("User-Agent", "Opera").asString
   	}
 }
