f4390e0c14fecbc2f718ef60fb5724eee97dd052
Felipe Pereira <felipe.pereira@grumft.com>
2014-12-22 17:48:33 -0200

    Salving embeds on Mysql

---
+++ src/main/scala/bot/Mysql.scala
@@ -0,0 +1,26 @@
+/**
+ * @package   Bot
+ * @author    Felipe Maziero <flpmaziero@gmail.com>
+ * @copyright 2014 Felipe Maziero
+ */
+
+package jdbc
+
+import java.sql.{Connection,DriverManager}
+import com.mysql.jdbc
+
+class Mysql(host: String, database: String, username: String, password: String) {
+    val driver = "com.mysql.jdbc.Driver"
+    val url    = s"jdbc:mysql://$host:3306/$database"   
+    var connection:Connection = _
+    try {
+        Class.forName(driver)
+        connection = DriverManager.getConnection(url, username, password)
+    } catch {
+        case e: Exception => e.printStackTrace
+    }
+
+    def getConnection(): Connection = connection
+
+    def closeConnection(): Unit = connection.close
+}
