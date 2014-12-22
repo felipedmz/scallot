8b2b553fe81b07a5f78e11374e7965750fb38339
Felipe Pereira <felipe.pereira@grumft.com>
2014-12-22 16:23:45 -0200

    Cleaning code

--- src/main/scala/bot/Bot.scala
+++ src/main/scala/bot/Bot.scala
@@ -2,33 +2,31 @@
  * @package   Boot
  * @author    Felipe Maziero <flpmaziero@gmail.com>
  * @copyright 2014 Felipe Maziero
- *
- * Commented code for pedagogical purposes :D
  */
 
 package bot
 
-import scalaj.http._    // it's a java lib wrapped
-import scala.io.Source  // to read physical files
+import scalaj.http._
+import scala.io.Source
 
 object Bot {
 	var ways: List[String] = List()
 	var embeds: List[String] = List()
 
 	def main(args: Array[String]) {
-		println("\nInitialized reading of primary sources")
+		println("\nReading primary sources")
 		loadPrimarySources
 
-		println("\nSearching for compatible embeds")
+		println("\nSearching for embeds")
 		embeds = searchForEmbeds(ways)
   	}
 
   	def searchForEmbeds(urls: List[String]): List[String] = {
-  		for (url <- ways) { // this is like a foreach
+  		for (url <- ways) {
   			try {
 		  		val html  = getRequest(url)
-		  		val embed = """<embed(.)*>(.)*</embed>""".r // this is a Regex
-				val found = (embed findAllIn html).toList   // syntax sugar for "embed.findAllIn(html)"
+		  		val embed = """<embed(.)*>(.)*</embed>""".r
+				val found = (embed findAllIn html).toList
 		  		for (e <- found) {
 					println(e)
 					println("\n")
@@ -37,10 +35,10 @@ object Bot {
 				case error: Exception => println(s"$error >>> $url \n")
 			}
 		}
-  		urls // the last line is the return
+  		urls
   	}
 
-  	def loadPrimarySources(): Unit = { // Unit is a Scala's 'void'
+  	def loadPrimarySources(): Unit = {
   		for (line <- Source.fromFile("sources.txt").getLines()) {
 			val result = getRequest(line)
 			findLinks(result)
@@ -50,12 +48,10 @@ object Bot {
   	def findLinks(html: String): Unit = {
   		val link  = """http://[a-z0-9./?!@#$%&*)(-_=+;:,\|^~}{]*""".r
 		val found = (link findAllIn html).toList
-		ways = ways union found // there is a sum of lists (Yes, easy as you see ;D)
+		ways = ways union found
   	}
 
   	def getRequest(url: String): String = {
-  		/* How Http is a wrapped lib, this sintax sounds like Java, 
-  		   with the function returning directly the last line */
   		Http(url)
   			.option(HttpOptions.connTimeout(2000))
   			.option(HttpOptions.readTimeout(5000))
